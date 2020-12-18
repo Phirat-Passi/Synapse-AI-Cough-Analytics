9e73e7633b465e3b5fa5c79f698f5bc2adb6cc06
Phirat <67471222+Phirat-Passi@users.noreply.github.com>
2020-12-19 02:16:43 +0530

    Create feature extractor.py

---
+++ feature extractor.py
@@ -0,0 +1,20 @@
+# librosa feature extraction
+import librosa
+import numpy as np
+
+# Run all the sound files get all the feature data and write it to
+# a csv file
+
+audio = "audio.wav"
+y, sr = librosa.load(audio, mono=True, duration=1)
+# features
+chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
+spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
+rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+zcr = librosa.feature.zero_crossing_rate(y)
+mfcc = librosa.feature.mfcc(y=y, sr=sr)
+
+# printing the data
+for feature in (chroma_stft, spec_cent, spec_bw, rolloff, zcr, mfcc):
+    print(np.mean(feature))
